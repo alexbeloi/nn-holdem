c4edebce4cde5371e29814e99c9bf0cf88d632ab
alexbeloi <beloi.alex@gmail.com>
2015-09-02 17:58:50 -0700

    added two-way client/server communivation

--- holdem-player.py
+++ holdem-player.py
@@ -2,44 +2,139 @@ import argparse
 import threading
 import xmlrpc.client
 import time
+import math
 
 # table_spec['player'][#] is tuple ((int) stack, (bool) playing, (bool) betting)
 def my_state(table_spec):
     players = table_spec.get('players', None)
+    pocket_cards = table_spec.get('pocket_cards', None)
     pot = table_spec.get('pot', None)
-    position = table_spec.get('position', None)
+    button = table_spec.get('button', None)
+    tocall = table_spec.get('tocall', None)
 
 
 
 class PlayerControl(threading.Thread)
-    def __init__(self, threadID, name, stack=100000, playing=True, host, port):
-        super(Player, self).__init__()
-        address = "http://%s:%s" % (host, port)
-        self._server = xmlrpc.client.ServerProxy(address)
+    def __init__(self, threadID, name, stack=2000, playing=True, host, port, ai_flag=True):
+        super(PlayerControl, self).__init__()
+        self._server = xmlrpc.client.ServerProxy('http://localhost:8000')
         self.daemon = True
-        self._name = name
-        self._threadID = threadID
 
-        self._hand = []
+        self._ai_flag = ai_flag
+        self._name = name
+        self._host = host
+        self._port = port
+        self._threadID = threadID
         self._stack =  stack
+        self._hand = []
 
     def run(self):
-        self._server.add_player(self._threadID, self._name, self._stack)
+        self._server.add_player(self._threadID, self._name, self._stack, self._host, self._port)
+        table_state = self._server.get_table_state(self._threadID)
         while True:
-            table_state = self._server.get_table_state()
+            table_state_new = self._server.get_table_state(self._threadID)
+            if table_state_new != table_state:
+                table_state = table_state_new
+                self.show_table(table_state)
+
             if not table_state:
                 print("not in hand... waiting")
                 time.sleep(10)
                 continue
 
+    def show_table(self, table_spec):
+
+    def update_localstate(table_state):
+        self._stack = table_state.get('stack')
+        self._hand = table_state.get('pocket')
+
+    def player_move(self, table_state):
+        update_localstate(table_state)
+        show_table(table_state)
+        tocall = table_state.get('tocall')
+
+        if not self._ai_flag:
+            if tocall == 0:
+                print "1) Raise"
+                print "2) Check"
+                choice = input("Choose your option: ")
+                if choice == 1:
+                    choice2 = input("How much would you like to raise? (min = {}, max = {})".format(tocall,self._stack))
+                    while choice2 < tocall:
+                        choice2 = input("(Invalid input) How much would you like to raise? (min = {}, max = {})".format(tocall,self._stack))
+                    move = ('raise',min(choice2, self._stack))
+                    self._stack -= choice2
+                if choice ==2:
+                    move = ('check', 0)
+            else:
+                print "1) Raise"
+                print "2) Call"
+                print "3) Fold"
+            choice = input("Choose your option: ")
+            if choice == 1:
+                choice2 = input("How much would you like to raise? (min = {}, max = {})".format(tocall,self._stack))
+                while choice2 < tocall:
+                    choice2 = input("(Invalid input) How much would you like to raise? (min = {}, max = {})".format(tocall,self._stack))
+                move = ('raise',min(choice2, self._stack))
+                self._stack -= choice2
+            elif choice == 2:
+                move = ('call', tocall)
+                self._stack -= tocall
+            elif choice == 3:
+                move = ('fold', -1)
+            return move
+        else:
+            bet_size = nnholdem(table_state)
+            if bet_size < tocall:
+                if bet_size >= self._stack:
+                    move = ('allin', self._stack)
+                else:
+                    move = ('fold',-1)
+            elif bet_size > tocall:
+                bet_size -= bet_size % table_state.get('bigblind')
+                if bet_size >tocall:
+                    self._stack -= min(bet_size, self._stack)
+                    move = ('raise', min(bet_size, self._stack))
+                else:
+                    move = ('call', tocall)
+            elif bet_size = tocall:
+                self._stack -= tocall
+                move = ('call', tocall)
+            return move
+
+
+class PlayerProxy(object):
+    def __init__(self,player):
+        self._player = player
+
+    def player_move(self, output_spec):
+
+    def get_table_state(self, threadID):
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    # parser.add_argument("num_players", type=int, default=1)
+    parser.add_argument("id", type=str, default="1")
     parser.add_argument("host", type=str, default="localhost")
     parser.add_argument("port", type=str, default="8000")
+    parser.add_argument("ai", type=bool, default=True)
     args = parser.parse_args()
 
-    #replace 1 with a uniquely generated id
-    player = Player(1, "Alice", args.host, args.port)
+    port = args.port + args.id
+
+    player = Player(args.id, "Alice", args.host, port, args.ai)
+    player_proxy = PlayerProxy(player)
+
     player.start()
     player.join()
+
+    server = SimpleXMLRPCServer((chost, cport), logRequests=False, allow_none=True)
+    # server = SimpleXMLRPCServer(("0.0.0.0", 8000), Handler)
+    server.register_instance(player_proxy, allow_dotted_names=True)
+    # server.register_introspection_functions()
+    try:
+        server.serve_forever()
+    except KeyboardInterrupt:
+        print("\nKeyboard interrupt received, exiting.")
+        server.server_close()
+        sys.exit(0)
--- holdem.py
+++ holdem.py
@@ -3,26 +3,71 @@ import threading
 import xmlrpc.client
 import time
 
-class Table(object):
-    def __init__(self, blinds=True, rake=False):
+class Table(threading.Thread):
+    def __init__(self, seats = 8, blinds = True, sb = 10, bb = 25, host, port):
+        super(Table, self).__init__()
+        self._smallblind = sb
+        self._bigblind = bb
+        self._deck = list(range(52))
+
         self._round = 0
-        self._position = 0
+        self._button = 0
         self._discard = []
         self._community = []
         self._pot = 0
-        self._deck = list(range(52))
-        self._players = []
-        self._thread_registry = []
+        self._tocall = 0
+
+        self._seats = [None]*seats
+        self._player_dict = {}
 
         self._lock = threading.Lock()
 
+    def run(self):
+        players = [player for player in self._seats if player not None]
+        players_playing = [player for player in players if player.playing()]
+        button_idx = players_playing.index(self._seats[self._button])
+        while len(players_playing)>1:
+            current_player = button_idx
+            current_player = current_player +1 % len(players_playing)
+
+            # small blind
+            players_playing[current_player].bet(self._smallblind)
+            self.pot += self._smallblind
+
+            current_player = current_player +1 % len(players_playing)
+            # big blind
+            players_playing[current_player].bet(self._bigblind)
+            self.pot += self._bigblind
+            self._tocall = self._bigblind
+
+            self.deal()
+            current_player = current_player +1 % len(players_playing)
+            while current_player != button_idx+2:
+                state = get_table_state(players_playing[current_player])
+                move = players_playing[current_player].server.player_move(state)
+                print move
+                if move[1] >= 0:
+                    players_playing[current_player].bet(move[1])
+                    self._pot += move[1]
+                    if move[1] > tocall:
+                        tocall = move[1]
+                elif move[1] <0:
+
+                current_player = current_player +1 % len(players_playing)
+
+
+
+
+
     def deal(self):
-        for player in self._players:
-            if player.playing():
-                player.addtohand(_get_card())
-        for player in self._players:
-            if player.playing():
-                player.addtohand(_get_card())
+        for player in self._seats:
+            if player not None:
+                if player.playing():
+                    player.addtohand(self._get_card())
+        for player in self._seats:
+            if player not None:
+                if player.playing():
+                    player.addtohand(self._get_card())
 
     def flop(self):
         self._discard.append(self._get_card()) #burn
@@ -43,37 +88,48 @@ class Table(object):
         del self._deck[n]
         return card
 
-    def add_player(self, threadID, name, stack):
-        if threadID not in self._thread_registry:
-            player = Player(threadID, name, stack)
-            self._players.append[player]
-            self._thread_registry.append[threadID]
+    def add_player(self, threadID, name, stack, host, port):
+        if threadID not in self._player_dict:
+            player = Player(threadID, name, stack, host, port)
+            idx = self._seats.index(None)
+            self._seats[idx] = player
+            self._player_dict[threadID] = player
 
     def remove_player(self, threadID):
-        if threadID in self._thread_registry:
-            self._thread_registry.remove(threadID)
+        if threadID in self._player_dict:
+            self._seats.remove(self._player_dict[threadID])
+            del self._player_dict[threadID]
         except ValueError:
             pass
 
     def reset(self):
-        for player in self._players:
-            player.reset_hand()
-
-        self._position = (self._position + 1) % len(self._players)
-        while not self._players[self._position].playing():
-            self._position = (self._position + 1) % len(self._players)
+        for player in self._seats:
+            if player not None:
+                player.reset_hand()
+        self._deck = range(range(52))
+        self._button = (self._button + 1) % len(self._seats)
+        while not self._seats[self._button].playing():
+            self._button = (self._button + 1) % len(self._seats)
 
     def output_state(self, threadID):
         with self._lock:
-            return {'players':[player.player_state() for player in players if player.threadID != threadID], 'pot' = self._pot, 'position' = self._position}
+
+            return {'players':[player.player_state() for player in players if player.threadID != threadID], 'pocket_cards':self._player_dict[threadID].pocket_cards() 'pot':self._pot, 'button':self._button, 'tocall':self._tocall, 'stack':self._player_dict[threadID].stack 'bigblind':self._bigblind}
 
 class Player(object):
-    def __init__(self, threadID, name, stack):
+    def __init__(self, threadID, name, stack, host, port):
+        self._host
+        self._port
         self._hand = []
-        self._stack = stack
+        self.stack = stack
+        self.bet = 0
+        self._isallin = False
         self._isplaying = True
         self._isbetting = False
+        self.myturn = False
         self.threadID = threadID
+        address = "http://%s:%s" % (host, port)
+        self.server = xmlrpc.client.ServerProxy(address)
 
     def addtohand(self, card):
         self._hand.append(card)
@@ -81,11 +137,116 @@ class Player(object):
     def reset_hand(self):
         self._hand=[]
 
+    def pocket_cards(self):
+        return self._hand
+
     def playing(self):
         return self._isplaying
+
+    def bet(self, bet_size):
+        self.stack -= bet_size
+        self.bet = bet_size
+        if self.stack <= 0:
+            self._isallin = True
+
+    def refund(self, ammount):
+        self.stack += ammount
 
     def betting(self):
         return self._isbetting
 
     def player_state(self):
-        return (self._stack, self.playing(), self.betting())
+        return (self.stack, self.playing(), self.betting())
+
+class TableProxy(object):
+    def __init__(self, table):
+        self._table = table
+    def get_table_state(self, threadID):
+        return table.output_state(self, threadID):
+
+    def player_move(self, threadID, result):
+
+if __name__ = '__main__':
+    parser = argparse.ArgumentParser()
+    # parser.add_argument("num_players", type=int, default=1)
+    parser.add_argument("host", type=str, default="localhost")
+    parser.add_argument("port", type=str, default="8000")
+    args = parser.parse_args()
+
+    print "Welcome to the Texas Holdem table, how many seats would you like at this table? (default=8)"
+    print
+    default_yesno = input("default configuration? (y/n) ")
+    if default_yesno[0] == 'y':
+        table = Table(args.host, args.port)
+    else:
+        seats = input("Number of seats: ")
+
+        print "Would you like to play with blinds or antes? (default=blinds)"
+        print "1) Blinds"
+        print "2) Antes"
+        blind_or_ante = input("Choose your option: ")
+        if blind_or_ante == 1:
+            blinds = True
+            print "How large should the small-blind/big-blind be? (default = 10/25)"
+            print "1) 10/25"
+            print "2) 25/50"
+            print "3) 50/100"
+            print "4) 100/200"
+            blind_size = input("Choose your option: ")
+            if blind_size == 1:
+                sb = 10
+                bb = 25
+            elif blind_size == 2:
+                sb = 25
+                bb = 50
+            elif blind_size == 3:
+                sb = 50
+                bb = 100
+            elif blind_size == 4:
+                sb = 100
+                bb = 200
+            else:
+                print("Invalid input, defaulting to 10/25 blinds.")
+                sb = 10
+                bb = 25
+        elif blind_or_ante == 2:
+            blinds = False
+            print "How big should the antes be? (default=50)"
+            print "1) 10"
+            print "2) 25"
+            print "3) 50"
+            print "4) 100"
+            ante_size = input("Choose your option: ")
+            if ante_size == 1:
+                ante_ammount = 10
+            elif ante_size == 2:
+                ante_ammount = 25
+            elif ante_size == 3:
+                ante_ammount = 50
+            elif ante_size == 4:
+                ante_ammount = 100
+            else:
+                print "Invalid option, defaulting to ante size = 50"
+                ante_ammount = 50
+        else:
+            print "Invalid input, defaulting to 10/25 blinds."
+            blinds = True
+            sb = 10
+            bb = 25
+
+        try:
+            table = Table(seats, blinds, sb, bb, args.host, args.port)
+        except TypeError:
+            table = Table(blinds, sb, bb, args.host, args.port)
+
+    table_proxy = TableProxy(table)
+
+    server = SimpleXMLRPCServer(("localhost", 8000), logRequests=False, allow_none=True)
+    server.register_instance(table_proxy, allow_dotted_names=True)
+
+    try:
+        server.serve_forever()
+    except KeyboardInterrupt:
+        print("\nKeyboard interrupt received, exiting.")
+        server.server_close()
+        sys.exit(0)
