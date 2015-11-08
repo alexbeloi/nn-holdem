94e2f67cd53ecd87e4e3511aa3adf6bd59d2aa1d
alexbeloi <beloi.alex@gmail.com>
2015-11-08 10:29:36 -0800

    fix main

--- analyzer_network_training.py
+++ analyzer_network_training.py
@@ -3,46 +3,48 @@ import random
 from holdem import Analyzer, NeuralNetwork, HoldemAI
 from deuces.deuces import Card, Deck, Evaluator
 
-a = Analyzer()
-a.monte_carlo_rounds = 2000
 
-nn = NeuralNetwork([206,206,100,1], 'analyzer_network', 0.1)
+if __name__ = '__main__':
+    a = Analyzer()
+    a.monte_carlo_rounds = 2000
 
-deck = Deck()
-hand = []
-community = []
-opponents = 0
-game_stage = [0,3,4,5]
+    nn = NeuralNetwork([206,206,100,1], 'analyzer_network', 0.1)
 
-for i in range(1000000):
+    deck = Deck()
+    hand = []
     community = []
-    deck.shuffle()
-    a.reset()
+    opponents = 0
+    game_stage = [0,3,4,5]
 
-    hand = deck.draw(2)
-    for _ in range(game_stage[random.randint(0,3)]):
-        community.append(deck.draw(1))
-    opponents = random.randint(1,7)
+    for i in range(1000000):
+        community = []
+        deck.shuffle()
+        a.reset()
 
-    a.set_num_opponents(opponents)
-    a.set_pocket_cards(*hand)
-    for card in community:
-        a.community_card(card)
+        hand = deck.draw(2)
+        for _ in range(game_stage[random.randint(0,3)]):
+            community.append(deck.draw(1))
+        opponents = random.randint(1,7)
 
-    mc_response = a.analyze()
-    # print('mc response', mc_response)
+        a.set_num_opponents(opponents)
+        a.set_pocket_cards(*hand)
+        for card in community:
+            a.community_card(card)
 
-    hand_bin = [j for i in [HoldemAI.card_to_binlist(c) for c in hand] for j in i]
-    community = community + [0]*(5-len(community))
-    comm_bin = [j for i in [HoldemAI.card_to_binlist(c) for c in community] for j in i]
-    opponents_bin = HoldemAI.bin_to_binlist(bin(opponents)[2:].zfill(3))
+        mc_response = a.analyze()
+        # print('mc response', mc_response)
 
-    inputs = HoldemAI.center_bin(hand_bin + comm_bin + opponents_bin)
+        hand_bin = [j for i in [HoldemAI.card_to_binlist(c) for c in hand] for j in i]
+        community = community + [0]*(5-len(community))
+        comm_bin = [j for i in [HoldemAI.card_to_binlist(c) for c in community] for j in i]
+        opponents_bin = HoldemAI.bin_to_binlist(bin(opponents)[2:].zfill(3))
 
-    # print('nn response', (1+nn.activate(inputs))/2)
-    print('error: ', 100*((nn.activate(inputs)+1)/2 - mc_response))
-    nn.update_weights_linesearch(inputs, 2*mc_response-1)
+        inputs = HoldemAI.center_bin(hand_bin + comm_bin + opponents_bin)
 
-    if (i % 100) == 0:
-        print('SAVING ON ITERATION: ', i)
-        nn.save()
+        # print('nn response', (1+nn.activate(inputs))/2)
+        print('error: ', 100*((nn.activate(inputs)+1)/2 - mc_response))
+        nn.update_weights_linesearch(inputs, 2*mc_response-1)
+
+        if (i % 100) == 0:
+            print('SAVING ON ITERATION: ', i)
+            nn.save()
